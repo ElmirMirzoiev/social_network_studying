173a6d1c3c5987a6ac95e74ee7386133b2e69c31
ElmirMirzoiev <elmir.mirzoiev@outlook.com>
2022-01-04 22:32:05 +0100

    UserAPI CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./component/Header/Header";
+import Navbar from "./component/Navbar/Navbar";
+import Profile from "./component/Profile/Profile";
+import Music from "./component/Music/Music";
+import Settings from "./component/Settings/Settings";
+import {Routes, Route } from "react-router-dom";
+import MessagesContainer from "./component/Messages/MessagesContainer";
+import UsersContainer from "./component/Users/UsersContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (<div className='container'>
+        <Header/>
+        <Navbar store={props.store}/>
+        <div className='app-content'>
+            <Routes>
+                <Route path='/profile' element={<Profile store={props.store}/>}/>
+                <Route path='/messages' element={<MessagesContainer store={props.store}/>}/>
+                <Route path='/music' element={<Music/>}/>
+                <Route path='/settings' element={<Settings/>}/>
+                <Route path='/users' element={<UsersContainer store={props.store}/>}/>
+                {/*<Route path='/friends' element={<Friends/>}/>*/}
+            </Routes>
+        </div>
+    </div>);
 }
 
 export default App;
