086bf941c8816b91671a88f427663aec4df752c9
ElmirMirzoiev <elmir.mirzoiev@outlook.com>
2022-01-22 00:19:44 +0100

    Refactoring

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -1,70 +1,65 @@
-import {IS_AUTH, SET_AUTH_DATA} from "../types";
-import {authAPI} from "../../API/authAPI";
-
+import { IS_AUTH, SET_AUTH_DATA } from "../types"
+import { authAPI } from "../../API/authAPI"
 
 const initialState = {
-    email: null,
-    login: null,
-    id: null,
-    isAuth: false
-};
+  email: null,
+  login: null,
+  id: null,
+  isAuth: false,
+}
 
 const authReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case SET_AUTH_DATA:
-            return {
-                ...state,
-                ...action.payload,
-                isAuth: true
-            }
-        case IS_AUTH:
-            return {
-                ...state,
-                isAuth: false
-            }
-        default:
-            return state
-    }
-};
+  switch (action.type) {
+    case SET_AUTH_DATA:
+      return {
+        ...state,
+        ...action.payload,
+        isAuth: true,
+      }
+    case IS_AUTH:
+      return {
+        ...state,
+        isAuth: false,
+      }
+    default:
+      return state
+  }
+}
 
-export const setAuthData = (payload) => ({type: SET_AUTH_DATA, payload});
-export const setLogOut = () => ({type: IS_AUTH});
-
+export const setAuthData = (payload) => ({ type: SET_AUTH_DATA, payload })
+export const setLogOut = () => ({ type: IS_AUTH })
 
 export const setAuthDataThunk = () => {
-    return (dispatch) => {
-        authAPI.me().then(response => {
-            if (response.resultCode === 0) {
-                dispatch(setAuthData(response.data))
-
-            }
-            if (response.resultCode === 1) {
-                console.log(response.messages)
-            }
-        })
-    }
-};
+  return (dispatch) => {
+    authAPI.me().then((response) => {
+      if (response.resultCode === 0) {
+        dispatch(setAuthData(response.data))
+      }
+      if (response.resultCode === 1) {
+        console.log(response.messages)
+      }
+    })
+  }
+}
 
 export const logInThunk = (email, password) => {
-    return dispatch => {
-        authAPI.login(email, password).then(response => {
-            if (response.resultCode === 0) {
-                dispatch(setAuthDataThunk())
-            } else {
-                console.log(response.messages)
-            }
-        })
-    }
+  return (dispatch) => {
+    authAPI.login(email, password).then((response) => {
+      if (response.resultCode === 0) {
+        dispatch(setAuthDataThunk())
+      } else {
+        console.log(response.messages)
+      }
+    })
+  }
 }
 
 export const logOutThunk = () => {
-    return dispatch => {
-        authAPI.logout().then(response => {
-                dispatch(setLogOut())
-            }
-        )
-    }
+  return (dispatch) => {
+    authAPI.logout().then((response) => {
+      dispatch(setLogOut())
+    })
+  }
 }
 
-
-export default authReducer;
+export default authReducer
